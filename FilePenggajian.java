045f02e7af42d4e056a4404298a4bd40a281e38a
Willy-557 <williamfsetiadi@gmail.com>
2025-12-26 18:05:32 +0700

    Make the menu information

---
+++ FilePenggajian.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+public class FilePenggajian {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, Karyawan> dataKaryawan = new HashMap<>();
+
+        while (true) {
+            System.out.println("");
+        }
+        
+    }
+}
+
